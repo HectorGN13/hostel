6d00871b632dcc60d8be96358c170beddda20e30
Héctor E. García Núñez <hectoreduardo.garcia@iesdonana.org>
2020-05-25 18:05:14 +0200

    Instalado composer

--- config/web.php
+++ config/web.php
@@ -16,7 +16,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'QNPDoE_I_EapWSsZjVQ0HBuj914f01SS',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',

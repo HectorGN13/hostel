39d03e821fecd149a5da8efe9c0799e77e28e668
Héctor E. García Núñez <hectoreduardo.garcia@iesdonana.org>
2020-05-25 18:27:54 +0200

    Añadido el crud de cliente

---
+++ views/customer/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\bootstrap4\Html;
+use yii\bootstrap4\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Customer */
+/* @var $form yii\bootstrap4\ActiveForm */
+?>
+
+<div class="customer-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'surname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phone_number')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
